2105323916b053b40166b41299f83bc61a953f38
Radik Abubjazov <abubjazov@mail.ru>
2021-12-17 23:43:26 +0300

    App/index.js: +todoRoutes

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const path = require('path')
 const app = express()
 const PORT = process.env.PORT
 
+const todoRoutes = require('./routes/todo')
+
 app.use(express.static(path.join(__dirname, 'public')))
+
+app.use('/api/todo', todoRoutes)
 
 app.use((req, res, next) => {
     res.sendFile('/index.html')
