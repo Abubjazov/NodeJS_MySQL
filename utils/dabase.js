f366536b174c2fef1035d6899a179d0c52f12627
Radik Abubjazov <abubjazov@mail.ru>
2021-12-18 00:54:15 +0300

    App: set up mysql access

--- utils/dabase.js
+++ utils/dabase.js
@@ -1,7 +1,5 @@
 const Sequelize = require('sequelize')
 
-
-
 const sequelize = new Sequelize(process.env.DB_NAME, process.env.DB_USER_NAME, process.env.DB_USER_PASSWORD, {
     host: process.env.HOST,
     dialect: process.env.DB_DIALECT
