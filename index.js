2bbe26494b3cec99a3a5678250410d9b6779589a
Radik Abubjazov <abubjazov@mail.ru>
2021-12-17 23:31:08 +0300

    App: base setup

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
 require('dotenv').config()
 const express = require('express')
+const path = require('path')
 const app = express()
 const PORT = process.env.PORT
 
-app.listen(PORT)
+app.use(express.static(path.join(__dirname, 'public')))
+
+app.use((req, res, next) => {
+    res.sendFile('/index.html')
+})
+
+app.listen(PORT, () => {
+    console.log(`Server is running on PORT: ${PORT}`)
+})
