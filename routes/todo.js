e6b374b63b47d69548511753fabd4031a12929dd
Radik Abubjazov <abubjazov@mail.ru>
2021-12-18 14:35:54 +0300

    App: add Route content - /routes/todo.js

--- routes/todo.js
+++ routes/todo.js
@@ -3,9 +3,11 @@ const router = Router()
 
 const Todo = require('../models/todo')
 
-router.get('/', (req, res) => {
+router.get('/', async (req, res) => {
     try {
+        const todos = await Todo.findAll()
 
+        res.status(200).json(todos)
 
     } catch (error) {
         console.log(error)
