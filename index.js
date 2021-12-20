3877ce2601e637973c5f183e32b3da32a8fe4983
Radik Abubjazov <abubjazov@mail.ru>
2021-12-20 15:50:40 +0300

    GraphQL Start point

--- index.js
+++ index.js
@@ -6,12 +6,8 @@ const sequelize = require('./utils/dabase')
 
 const PORT = process.env.PORT
 
-const todoRoutes = require('./routes/todo')
-
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(express.json())
-
-app.use('/api/todo', todoRoutes)
 
 app.use((req, res, next) => {
     res.sendFile('/index.html')
